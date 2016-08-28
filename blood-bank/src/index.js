6833dc9c5136ae85caf9c768b60b2395f79017d0
Paul Adams <Paul Adams>
2016-08-28 16:59:14 -0400

    initial commit

---
+++ blood-bank/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BloodBankApp from './BloodBankApp';
+import './index.css';
+
+ReactDOM.render(
+  <BloodBankApp />,
+  document.getElementById('root')
+);

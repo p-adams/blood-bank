8abf2b4ada739849c8bf2740563392ea4bf15974
Paul Adams <Paul Adams>
2016-08-28 19:54:15 -0400

    updated components

--- blood-bank/src/CreateUser.js
+++ blood-bank/src/CreateUser.js
@@ -7,6 +7,17 @@ var CreateUser = React.createClass({
                 <form>
                     <input onChange={this.props.getFirst} placeholder="firstname..."/>
                     <input onChange={this.props.getLast} placeholder="lastname..."/>
+                    <input onChange={this.props.getContact} placeholder="phone number or email"/>
+                    <label>Blood Group</label>
+                    <select>
+                        <option>A</option>
+                        <option>B</option>
+                        <option>AB</option>
+                        <option>O</option>
+                    </select>
+                    <label>RH Factor</label>
+                     <input type="radio" name="rh-factor" value="negative"/>Negative<br/>
+                     <input type="radio" name="rh-factor" value="positive"/>Positive<br/>
                     <span>Already a user? <a href="#" onClick={this.props.login}>Login</a></span>
                     <button onClick={this.props.storeUser}>Create User</button>
                 </form>

21ac9dd83d615d17fb33c5ead3c3e3462abecbfb
Paul Adams <Paul Adams>
2017-03-10 18:56:40 -0500

    refactored CreateUser component

--- src/CreateDonor.js
+++ src/CreateDonor.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 
-var CreateUser = React.createClass({
-    render: function(){
+class CreateUser extends Component{
+    render(){
         return(
                 <form>
                 <div className="form-group">
